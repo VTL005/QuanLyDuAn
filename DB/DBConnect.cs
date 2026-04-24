d81535187d40f6ffeaa94bef0573ca118ad716c0
Mha <minhha28092005@gmail.com>
2026-04-24 13:38:55 +0700

    sua

--- DB/DBConnect.cs
+++ DB/DBConnect.cs
@@ -8,7 +8,7 @@ namespace QuanLyDuAn.DB
         private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=QuanLyDuAnDB;Integrated Security=True";
 
         public SqlConnection GetConnection()
-        {// toi la my an cut cho
+        {// toi la my an cut choa
             // toi la vy the long adu
             // xin chao tat ca moi nguoi
             return new SqlConnection(connectionString);
