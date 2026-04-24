aa343df7e8d68d5c8d6f201c96ad8038174adc44
My <2311062599@hunre.edu.vn>
2026-04-24 13:27:25 +0700

    sua file dbhtfghAdd prolject filelilisvbbvffbb.;

--- DB/DBConnect.cs
+++ DB/DBConnect.cs
@@ -7,7 +7,7 @@ namespace QuanLyDuAn.DB
         private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=QuanLyDuAnDB;Integrated Security=True";
 
         public SqlConnection GetConnection()
-        {
+        {// toi la my
             return new SqlConnection(connectionString);
         }
     }
